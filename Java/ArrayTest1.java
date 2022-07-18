c201bfdd1fc4c0d0af83948fd352f651bf00adfe
엉덩희 <82931413+easydong02@users.noreply.github.com>
2022-07-18 18:39:31 +0900

    Create ArrayTest1.java

---
+++ Java/ArrayTest1.java
@@ -0,0 +1,19 @@
+package ssafy0718;
+
+public class ArrayTest1 {
+
+	public static void main(String[] args) {
+		// 타입
+		// bsil fd b c
+		long a =10L;
+		long v = 0L+Integer.MAX_VALUE+10;
+		
+		
+		for(int i =0;i<50;i++) {
+			System.out.printf("%c ",('A'+1));
+		}
+		System.out.println();
+		
+	}
+
+}
