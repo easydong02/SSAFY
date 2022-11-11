1638156dfd2e248f09c664b83d71a0dcea26c9fc
easydong02 <easydong02@gmail.com>
2022-11-11 17:55:30 +0900

    vueboard1111

---
+++ November/week2/vueboard1111/src/components/ArticleItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-tr>
+    <b-td v-text="index + 1"></b-td>
+    <b-td v-text="article.subject"></b-td>
+    <b-td v-text="article.userId"></b-td>
+    <b-td>{{ article.registerTime }}</b-td>
+  </b-tr>
+</template>
+
+<script>
+export default {
+  name: "AritlceItem",
+  props: {
+    article: Object,
+    index: Number,
+  },
+};
+</script>
+
+<style></style>
---
+++ November/week2/vueboard1111/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <hello-world msg="인재군 인재리스트" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
